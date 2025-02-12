5c9558213fe261b4a385352d1a39f5d8ead99f80
Henry Gustavo X. Az <azhenrygus@outlook.es>
2025-02-12 00:23:49 -0600

    rename login and logout routes

--- routes/auth.php
+++ routes/auth.php
@@ -5,9 +5,9 @@ use App\Livewire\Pages\Auth\Login\Login;
 
 Route::get('/auth/login', Login::class)
     ->middleware('guest')
-    ->name('auth.login');
+    ->name('login');
 
 Route::post('/logout', function() {
     \Illuminate\Support\Facades\Auth::logout();
-    return redirect()->route('auth.login');
-})->name('auth.logout');
+    return redirect()->route('login');
+})->name('logout');
