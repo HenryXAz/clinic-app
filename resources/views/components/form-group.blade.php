c6e4ff4024cf0128288cb668cf9a2a2fbae98844
Henry Gustavo X. Az <azhenrygus@outlook.es>
2025-02-17 13:38:31 -0600

    add items center property to form group component

--- resources/views/components/form-group.blade.php
+++ resources/views/components/form-group.blade.php
@@ -1,5 +1,5 @@
 @props(['class' => ''])
 
-<div class="{{$class . ' w-full flex gap-2 flex-col md:flex-row justify-between'}}">
+<div class="{{$class . ' w-full flex gap-2 flex-col md:flex-row justify-between items-center'}}">
     {{$slot}}
 </div>
