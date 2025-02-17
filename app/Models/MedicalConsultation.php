b578feae8df9b26bd9fabf2444149569ed7b0330
Henry Gustavo X. Az <azhenrygus@outlook.es>
2025-02-17 13:34:34 -0600

    add reaseon abbreviation attribute and cast for date

--- app/Models/MedicalConsultation.php
+++ app/Models/MedicalConsultation.php
@@ -7,8 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class MedicalConsultation extends Model
 {
     protected $casts = [
-        'start_date' => 'datetime',
-        'end_date' => 'datetime',
+        'date' => 'datetime',
+//        'start_date' => 'datetime',
+//        'end_date' => 'datetime',
     ];
 
     protected $guarded = [
@@ -19,6 +20,18 @@ class MedicalConsultation extends Model
     protected function getConsultationDateAttribute() : string
     {
         return "$this->attributes['start_date'] - $this->attributes['end_date']";
+    }
+
+    protected function getReasonAbbreviationAttribute() : string
+    {
+        $value = substr($this->attributes['reason'], 0, 20);
+
+        if (strlen($value) >= 20) {
+            $value.='...';
+        }
+
+        return $value;
+//        return substr($this->attributes['reason'], 0, 20) . '...';
     }
 
 }
