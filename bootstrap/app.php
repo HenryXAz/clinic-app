0ce267364fe91e4784e4237627eea2d7a43e0ff6
Henry Gustavo X. Az <azhenrygus@outlook.es>
2025-11-09 22:45:19 -0600

    Add route files directly in bootstrap app file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,12 +3,26 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: function () {
+            Route::middleware('web')
+                ->group(base_path('/routes/auth.php'));
+
+            Route::middleware('web')
+                ->group(base_path('/routes/patients.php'));
+
+            Route::middleware('web')
+                ->group(base_path('/routes/consultations.php'));
+
+            Route::middleware('web')
+                ->group(base_path('/routes/documents.php'));
+        },
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
